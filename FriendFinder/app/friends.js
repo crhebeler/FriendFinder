844beea13eb84f5332a39e2608f29ec98f855e51
chrisheb1 <34869576+chrisheb1@users.noreply.github.com>
2018-05-14 15:16:57 -0400

    added

--- FriendFinder/app/friends.js
+++ FriendFinder/app/friends.js
@@ -1,1 +1,151 @@
-{\rtf1}
+[
+    {
+      "name": "BoB",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      "name": "Courtney",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+ 
+        "5",
+        "4",
+        "5",
+        "1",
+        "1",
+        "1",
+        "4",
+        "4",
+        "5",
+        "5"
+      ]
+    },
+    {
+      "name": "Jeff",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+
+        "2",
+        "3",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "3",
+        "4"
+      ]
+    },
+    {
+      "name": "Kristin",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+        "2",
+        "3",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "3",
+        "4"
+      ]
+    },
+    {
+      "name": "Milton",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "5",
+        "3",
+        "5",
+        "2",
+        "5",
+        "3"
+      ]
+    },
+    {
+      "name": "Megan",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+  
+      "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "5",
+        "3",
+        "5",
+        "2",
+        "5",
+        "3"
+      ]
+    },
+    {
+      "name": "Kristin",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+      "scores": [
+        "3",
+        "2",
+        "3",
+        "3",
+        "5",
+        "3",
+        "5",
+        "2",
+        "5",
+        "3"
+      ]
+    },
+    {
+      "name": "Sarah",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5XKF9HNvplFfwMzyM325bOyjdaszBz7aMDqcxBfOAvbCrKQ8W",
+      "location": "Orlando, FL, USA",
+      "email": "email@gmail.com",
+      "scores": [
+      "scores": [
+        "5",
+        "3",
+        "5",
+        "2",
+        "5",
+        "3",
+        "2",
+        "3",
+        "4",
+        "1"
+      ]
+    }
+  ]
